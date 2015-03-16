e8b8e2855634b1944cffc04ebceb0308288cf1b0
James Canady <jcanady20@gmail.com>
2015-03-15 23:14:48 -0500

    Initial git Checkin

---
+++ Menu.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestBench
+{
+	public class Menu
+	{
+		private int m_currentlocation = 0;
+		private List<MenuChoice> m_choices;
+		
+		public Menu()
+		{
+		}
+		public Menu(List<string> choices)
+		{
+			if (m_choices == null)
+				m_choices = new List<MenuChoice>();
+
+			m_choices.Clear();
+
+			foreach (string s in choices)
+			{
+				MenuChoice mc = new MenuChoice();
+				mc.Description = s;
+				m_choices.Add(mc);
+
+			}
+		}
+		public Menu(MenuChoice[] choices)
+		{
+			m_choices = choices.ToList();
+		}
+		public Menu(List<MenuChoice> choices)
+		{
+			m_choices = choices;
+		}
+
+		public List<MenuChoice> Choices
+		{
+			get
+			{
+				return m_choices;
+			}
+			set
+			{
+				m_choices = value;
+			}
+		}
+		public bool Canceled { get; private set; }
+
+		public void RunMenu()
+		{
+			bool _run = true;
+			while (_run)
+			{
+				Console.Clear();
+				int i = 0;
+				foreach (MenuChoice mc in m_choices)
+				{
+					if (i == m_currentlocation)
+					{
+						Console.BackgroundColor = ConsoleColor.Gray;
+						Console.ForegroundColor = ConsoleColor.Black;
+					}
+					else
+					{
+						Console.BackgroundColor = ConsoleColor.Black;
+						Console.ForegroundColor = ConsoleColor.White;
+					}
+					Console.WriteLine("{0}. [{1}]  {2}", i, (mc.Selected ? "X" : " "), mc.Description);
+					i++;
+				}
+
+				Console.BackgroundColor = ConsoleColor.Black;
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.WriteLine("");
+				Console.WriteLine("Press Enter to Execute");
+				Console.WriteLine("Press Q to Cancel");
+
+				ConsoleKeyInfo cki = Console.ReadKey(true);
+				switch (cki.Key)
+				{
+					case ConsoleKey.UpArrow:
+						if (m_currentlocation > 0)
+						{
+							--m_currentlocation;
+						}
+						break;
+					case ConsoleKey.DownArrow:
+						if (m_currentlocation < m_choices.Count -1)
+						{
+							++m_currentlocation;
+						}
+						break;
+					case ConsoleKey.Spacebar:
+						m_choices[m_currentlocation].Selected = !m_choices[m_currentlocation].Selected;
+						break;
+					case ConsoleKey.Q:
+						Canceled = true;
+						_run = false;
+						break;
+					case ConsoleKey.Enter:
+						_run = false;
+						break;
+				}
+				
+				System.Threading.Thread.Sleep(100);
+			}
+		}
+	}
+
+	public class MenuChoice
+	{
+		private Action _action;
+		public MenuChoice()
+		{ }
+		public MenuChoice(Action action)
+			: this()
+		{
+			_action = action;
+		}
+		public MenuChoice(string description)
+			: this()
+		{
+			Description = description;
+		}
+		public MenuChoice(Action action, string description)
+			: this()
+		{
+			_action = action;
+			Description = description;
+		}
+		public string Description { get; set; }
+		public bool Selected { get; set; }
+		public void Execute()
+		{
+			_action.Invoke();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,271 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace TestBench
+{
+    partial class Program
+    {
+		//	An internal pointed to the current Instance of the class
+		//	Used to reference Program and avoid using Activator Create Instance Calls
+		//	this will not work for static method calls
+		protected static Program m_this;
+		protected static string m_prompt = ":>";
+		protected static Type m_type = typeof(Program);
+		protected static List<MethodInfo> m_methods = null;
+        
+		internal Program()
+		{
+			m_this = this;
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			CreateMethodCache();
+			AddTraceListeners();
+
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.BufferHeight = 300;
+			Console.BufferWidth = 100;
+			Console.Title = "Test Bench";
+
+			if (args.Length > 0)
+			{
+				RunCommand(args);
+				return;
+			}
+
+			MainLoop();
+		}
+		static void MainLoop()
+		{
+			bool pContinue = true;
+			while (pContinue)
+			{
+				Console.Write(m_prompt);
+
+				//string[] Coms = Console.ReadLine().Split(new char[] { ' ' });
+				var parms = ParseCommand(Console.ReadLine());
+				if (parms.Length == 0)
+					continue;
+
+				switch (parms[0].ToLower())
+				{
+					case "exit":
+					case "quit":
+						pContinue = false;
+						break;
+					default:
+						RunCommand(parms);
+						break;
+				}
+				Console.WriteLine("");
+			}
+		}
+		static string[] ParseCommand(string args)
+		{
+			var parts = Regex.Matches(args, @"[\""].+?[\""]|[^ ]+")
+				.Cast<Match>()
+				.Select(x => x.Value.Replace("\"", ""))
+				.ToArray();
+			return parts;
+		}
+        static void RunCommand(string[] args)
+        {
+            string CallingMethod = args[0];
+			var parms = args.Skip(1).Take(args.Count() - 1).ToArray();
+
+			var method = m_methods.FirstOrDefault(x => String.Compare(x.Name, CallingMethod, true) == 0 && x.GetParameters().Length == parms.Length);
+			//	Abandon the call if we didn't find a match
+			if(method == null)
+			{
+				Console.WriteLine("Unknown Command");
+				Program.Help();
+				return;
+			}
+
+            try
+            {
+				method.Invoke(m_this, parms);
+                return;
+            }
+            catch (Exception e)
+            {
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine();
+                    WriteExceptions(e.InnerException);
+                }
+                return;
+            }
+        }
+
+        [Description("Shows Help for All Commands")]
+        static void Help()
+        {
+            Console.WriteLine("Valid Commands");
+			foreach (MethodInfo m in m_methods)
+            {
+                DescriptionAttribute[] attribs = (DescriptionAttribute[])m.GetCustomAttributes(typeof(DescriptionAttribute), false);
+				if (attribs != null && attribs.Length > 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(m.Name);
+                    ParameterInfo[] parm = m.GetParameters();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.Write("(");
+                    for (int i = 0; i < parm.Length; i++)
+                    {
+						if (i > 0)
+							Console.Write(", ");
+
+                        Console.Write("({0}){1}", parm[i].ParameterType.Name, parm[i].Name);
+                    }
+					Console.Write(")");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n\t{0}", attribs[0].Description);
+                }
+            }
+        }
+        [Description("Clears the Current display Buffer")]
+        static void Clear()
+        {
+            Console.Clear();
+        }
+        [Description("Quits out of the application")]
+        static void Quit()
+        {
+            return;
+        }
+		[Description("List Local Drives")]
+		static void LocalDrives()
+		{
+			string[] drives = Environment.GetLogicalDrives();
+			IEnumerable<string> strs = drives.Select(s => s.Replace(":\\", ""));
+			foreach (String s in strs)
+			{
+				System.IO.DriveInfo drvi = new System.IO.DriveInfo(s);
+				if (drvi.DriveType == DriveType.CDRom)
+					continue;
+				Console.WriteLine("{0}:\\", s);
+			}
+		}
+        [Description("List Available Providers")]
+        static void LocalProviders()
+        {
+            var dt = System.Data.Common.DbProviderFactories.GetFactoryClasses();
+            //	Name Description InvariantName
+            Console.WriteLine("{0} {1} {2}", "", "Name", "InvariantName");
+            Console.WriteLine("-------------------------------------");
+            foreach (System.Data.DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("{0} {1} {2}", "", dr["Name"], dr["InvariantName"]);
+            }
+        }
+		[Description("Open Application Log Folder")]
+		static void OpenLogFolder()
+		{
+			var path = Path.Combine(GetCurrentPath(), "ApplicationLogs");
+			if(Directory.Exists(path) == false)
+			{
+				path = GetCurrentPath();
+			}
+			Process.Start(path);
+		}
+        [Description("Menu Sample")]
+        static void SampleMenu()
+        {
+            List<MenuChoice> _choices = new List<MenuChoice>();
+
+            _choices.Add(new MenuChoice(new System.Action(Program.LocalDrives), "List Local Drives"));
+            _choices.Add(new MenuChoice(new System.Action(Program.OpenLogFolder), "Opens the Current Log Folder"));
+            _choices.Add(new MenuChoice(new System.Action(Program.Help), "Help"));
+            _choices.Add(new MenuChoice(new System.Action(Program.LocalProviders), "List Available Providers"));
+
+            Menu m = new Menu(_choices);
+
+            m.RunMenu();
+
+            if (m.Canceled)
+                return;
+
+            foreach (MenuChoice mc in _choices)
+            {
+                if (mc.Selected == false)
+                    continue;
+                mc.Execute();
+            }
+        }
+
+		static TimeSpan CalculateEta(DateTime startTime, int totalItems, int completeItems)
+		{
+			TimeSpan _eta = TimeSpan.MinValue;
+			//	Avoid Divide by Zero Errors
+			if (completeItems > 0)
+			{
+				int _itemduration = (int)DateTime.Now.Subtract(startTime).TotalMilliseconds / completeItems;
+				_eta = TimeSpan.FromMilliseconds((double)((totalItems - completeItems) * _itemduration));
+			}
+			return _eta;
+		}
+		static void WriteExceptions(Exception e)
+        {
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.Red;
+
+			Trace.Write("Source:");
+			Trace.Write(e.Source);
+			Trace.WriteLine("\nMessage:");
+			Trace.Write(e.Message);
+			Trace.WriteLine("\nStack Trace:");
+			Trace.Write(e.StackTrace);
+			Trace.WriteLine("\nUser Defined Data:");
+			foreach (System.Collections.DictionaryEntry de in e.Data)
+			{
+				Trace.WriteLine(string.Format("[{0}] :: {1}", de.Key, de.Value));
+			}
+			if (e.InnerException != null)
+			{
+				WriteExceptions(e.InnerException);
+			}
+			Console.ForegroundColor = ConsoleColor.White;
+        }
+		static string GetCurrentPath()
+		{
+			var asm = Assembly.GetExecutingAssembly();
+			var fi = new FileInfo(asm.Location);
+			return fi.DirectoryName;
+		}
+		static void HexDump(byte[] bytes)
+		{
+			for (int line = 0; line < bytes.Length; line += 16)
+			{
+				byte[] lineBytes = bytes.Skip(line).Take(16).ToArray();
+				System.Text.StringBuilder sb = new System.Text.StringBuilder();
+				sb.AppendFormat("{0:x8} ", line);
+				sb.Append(string.Join(" ", lineBytes.Select(b => b.ToString("x2")).ToArray()).PadRight(16 * 3));
+				sb.Append(" ");
+				sb.Append(new string(lineBytes.Select(b => b < 32 ? '.' : (char)b).ToArray()));
+				Console.WriteLine(sb);
+			}
+		}
+		static void CreateMethodCache()
+		{
+			m_methods = m_type.GetMethods(BindingFlags.Static | BindingFlags.NonPublic).ToList();
+		}
+		static void AddTraceListeners()
+		{
+			TextWriterTraceListener CWriter = new TextWriterTraceListener(Console.Out);
+			Trace.Listeners.Add(CWriter);
+		}
+    }
+}
